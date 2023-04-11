8d7609700e56a3c497bfc8ce30bbd85e9472ada1
annamelnyk <anna_melnyk@epam.com>
2023-04-11 14:56:34 +1000

    Reactive Design (courses retrieving)

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -17,28 +17,47 @@ import { createHttpObservable } from "../common/util";
   styleUrls: ["./home.component.css"],
 })
 export class HomeComponent implements OnInit {
-  beginnerCourses: Course[];
-  advancedCourses: Course[];
+  // imperative approach
+  // beginnerCourses: Course[];
+  // advancedCourses: Course[];
+
+  // Reactive Design
+  // data itsels is not available for direct mutation here
+  // we only have here definition of streams of data
+  beginnerCourses$: Observable<Course[]>;
+  advancedCourses$: Observable<Course[]>;
   constructor() {}
 
   ngOnInit() {
     const http$ = createHttpObservable("/api/courses");
 
-    const courses$ = http$.pipe(
+    const courses$: Observable<Course[]> = http$.pipe(
       map((response) => Object.values(response["payload"]))
     );
+
+    this.beginnerCourses$ = courses$.pipe(
+      map((courses) => {
+        return courses.filter((course: Course) => course.category === "BEGINNER");
+      })
+    );
+
+    this.advancedCourses$ = courses$.pipe(
+        map((courses) => {
+          return courses.filter((course: Course) => course.category === "ADVANCED");
+        })
+      );
 
     courses$.subscribe(
       // such approach increased code reading complexity
       // this is an imperative approach, preferable to use instaed Reactive Design
-      (courses: Course[]) => {
-        this.beginnerCourses = courses.filter(
-          (course: Course) => course.category === "BEGINNER"
-        );
-        this.advancedCourses = courses.filter(
-          (course: Course) => course.category === "ADVANCED"
-        );
-      },
+    //   (courses: Course[]) => {
+    //     this.beginnerCourses = courses.filter(
+    //       (course: Course) => course.category === "BEGINNER"
+    //     );
+    //     this.advancedCourses = courses.filter(
+    //       (course: Course) => course.category === "ADVANCED"
+    //     );
+    //   },
       noop, // noop === no operations
       () => console.log("completed")
     );
